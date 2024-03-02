4bc635360142ecfa6151c8c69d5eea825caff79c
esthercheung728 <esther_MayMay@yahoo.com.hk>
2024-03-02 22:44:44 +0800

    Added Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int numGuesses = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                numGuesses++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            Console.WriteLine($"You made {numGuesses} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            string playAgainInput = Console.ReadLine();
+
+            if (playAgainInput.ToLower() == "yes")
+            {
+                playAgain = true;
+                Console.WriteLine();
+            }
+            else
+            {
+                playAgain = false;
+            }
+        }
     }
 }
